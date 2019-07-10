b25c096399d31e2ae000762d3a6522f3f6f65dad
nehajoshi95 <50131877+nehajoshi95@users.noreply.github.com>
2019-07-10 13:44:03 +0530

    Update stripe.rb

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,6 +1,6 @@
 Rails.configuration.stripe = {
-  :publishable_key => 'pk_test_2N1vlSjttlf2QQWJkgQgruAN00DKxh4owS',
-  :secret_key      => 'sk_test_k0tA7AsH2aVUw1odIS6HpUB000O3KCvOWf'
+  :publishable_key => 'api-key',
+  :secret_key      => 'secret-key'
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
